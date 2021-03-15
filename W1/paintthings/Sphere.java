c331ec969406a9358e3117e39acf2751b78bf067
metardn <54663326+metardn@users.noreply.github.com>
2021-03-15 14:35:46 +0700

    Update Sphere.java

--- W1/paintthings/Sphere.java
+++ W1/paintthings/Sphere.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//*****************************************
+// Sphere.java
+//
+// Represents a sphere.
+//*****************************************
 package paintthings;
 
-/**
- *
- * @author acer
- */
 public class Sphere extends Shape{
     private double radius; //radius in feet
     //----------------------------------
