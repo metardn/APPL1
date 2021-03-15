7b503ec24ddc0426d3a138c433c92b805a69aa92
metardn <54663326+metardn@users.noreply.github.com>
2021-03-15 13:07:16 +0700

    Update Comission.java

--- W1/firm/Comission.java
+++ W1/firm/Comission.java
@@ -1,15 +1,13 @@
 //********************************************************************
 // Comission.java Author: Meta Arda Nabila
-//
-// 
 //********************************************************************
 package firm;
 
-public class Comission extends Hourly {
+public class Commission extends Hourly {
     double sales;       //total sales the employee has made
     double com_rate;    //comission rate for the employee
     
-    public Comission(String eName, String eAddress, String ePhone,
+    public Commission(String eName, String eAddress, String ePhone,
     String socSecNumber, double rate, double comission_rate){
         super(eName, eAddress, ePhone, socSecNumber, rate);
         com_rate = comission_rate;
@@ -17,5 +15,17 @@ public class Comission extends Hourly {
     
     public void addSales (double totalSales) {
         sales += totalSales;
+    }
+    
+    public double pay(){
+        double payment = super.pay();
+        payment += sales * com_rate;
+        return payment;
+    }
+    
+    public String toString() {
+        String result = super.toString();
+        result += "\nTotal Sales: " + sales;
+        return result;
     }
 }
