6027f74af2c2bf1ee177af655b82e8e7283749c7
metardn <54663326+metardn@users.noreply.github.com>
2021-03-18 19:01:52 +0700

    Update IntegerListTest.java

--- W1/integerlisttest/IntegerListTest.java
+++ W1/integerlisttest/IntegerListTest.java
@@ -9,6 +9,7 @@ package integerlisttest;
 import java.util.Scanner;
 
 public class IntegerListTest {
+    static IntegerList list = new IntegerList(10);
     static Scanner scan = new Scanner(System.in);
     
     //-------------------------------------------------------
@@ -16,6 +17,7 @@ public class IntegerListTest {
     // user asks until they quit
     //-------------------------------------------------------
     public static void main(String[] args) {
+        printMenu();
         int choice = scan.nextInt();
         while (choice != 0) {
             dispatch(choice);
@@ -53,11 +55,34 @@ public class IntegerListTest {
             case 4:
                 list.print();
                 break;
+            case 5:
+                System.out.println("Insert old element: ");
+                int oldElmt = scan.nextInt();
+                System.out.println("Insert new element: ");
+                int newElmt = scan.nextInt();
+                list.replaceFirst(oldElmt, newElmt);
+                break;
+            case 6:
+                System.out.println("Insert old element: ");
+                int oldElm = scan.nextInt();
+                System.out.println("Insert new element: ");
+                int newElm = scan.nextInt();
+                list.replaceAll(oldElm, newElm);
+                break;
+            case 7:
+                list.sortDecreasing();
+                break;
+            case 8:
+                System.out.println("Input the element of list");
+                int target = scan.nextInt();
+                list.binarySearchD(target);
+                break;
             default:
                 System.out.println("Sorry, invalid choice");
         }
+    }
         
-    //-------------------------------------------------------
+    //----------------------------------------------1---------
     // Print the user's choices
     //-------------------------------------------------------
     public static void printMenu() {
@@ -68,6 +93,10 @@ public class IntegerListTest {
         System.out.println("2: Sort the list using selection sort");
         System.out.println("3: Find an element in the list using linear search");
         System.out.println("4: Print the list");
+        System.out.println("5: Replace first occurrence in the list");
+        System.out.println("6: Replace all occurrence in the list");
+        System.out.println("7: Sort list into decreasing order");
+        System.out.println("8: Search descending list using binary search");
         System.out.print("\nEnter your choice: ");
     }
 }
