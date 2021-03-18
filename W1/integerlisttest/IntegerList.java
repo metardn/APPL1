733d59c3ef954c0422b8c51b1be0bb577f8cbc76
metardn <54663326+metardn@users.noreply.github.com>
2021-03-18 19:01:29 +0700

    Update IntegerList.java

--- W1/integerlisttest/IntegerList.java
+++ W1/integerlisttest/IntegerList.java
@@ -64,4 +64,63 @@ public class IntegerList {
             list[minIndex] = temp;
         }
     }
+    
+    //-------------------------------------------------------
+    //replaces the first occurrence of oldVal in the list with newVal
+    //-------------------------------------------------------
+    public void replaceFirst(int oldVal, int newVal) {
+        int i;
+        i = search(oldVal);
+        if(i > -1)
+            list[i] = newVal;
+    }
+    
+    //-------------------------------------------------------
+    //replaces the all occurrence of oldVal in the list with newVal
+    //-------------------------------------------------------
+    public void replaceAll(int oldVal, int newVal) {
+        for (int i = 0; i < list.length - 1; i++)
+            if (list[i] == oldVal)
+                list[i] = newVal;
+    }
+    
+    //-------------------------------------------------------
+    //sorts the list into decreasing
+    //-------------------------------------------------------
+    public void sortDecreasing() {
+        int maxIndex;
+        for (int i=0; i < list.length-1; i++) {
+            //find largest element in list starting at location i
+            maxIndex = i;
+            for (int j = i+1; j < list.length; j++)
+                if (list[j] > list[maxIndex])
+                    maxIndex = j;
+            //swap list[i] with largest element
+            int temp = list[i];
+            list[i] = list[maxIndex];
+            list[maxIndex] = temp;
+        }
+    }
+    
+    //-------------------------------------------------------
+    //find the target assuming the list is sorted in decreasing order
+    //-------------------------------------------------------
+    public int binarySearchD (int target){
+        int start = 0;
+        int fin = list.length-1;
+            
+        while (start <= fin){
+            int mid = (start + fin)/2;
+            if(target > list[mid])
+                fin = mid-1;
+            else if(target < list[mid])
+                start = mid+1;
+            else{
+                System.out.println( list[mid]+ " is found");
+                return mid;
+            }
+        }
+        System.out.println("element not found");
+        return 0;  
+    }
 }
