c0d53b02ddcce19b418abe71130e95ab05fca846
metardn <54663326+metardn@users.noreply.github.com>
2021-03-25 18:43:00 +0700

    Update Warning.java

--- W2/warning/Warning.java
+++ W2/warning/Warning.java
@@ -19,14 +19,17 @@ public class Warning {
         double qualityPts; // number of quality points earned
         double gpa; // grade point (quality point) average
         
-        String line, name, inputName = "students.dat";
-        String outputName = "warning.dat";
+        String line, name, inputName = "D:\\Polban\\Semester 4\\Analisis dan Perancangan Perangkat Lunak 1\\Praktek\\Java\\w2\\Warning\\src\\warning\\students.dat";
+        String outputName = "D:\\Polban\\Semester 4\\Analisis dan Perancangan Perangkat Lunak 1\\Praktek\\Java\\w2\\Warning\\src\\warning\\warning.dat";
         
         try {
             
             // Set up scanner to input file
-
+            Scanner scan = new Scanner(new File(inputName));
+            
             // Set up the output file stream
+            File outfile = new File(outputName);
+            PrintWriter outFile = new PrintWriter(outfile);
 
             // Print a header to the output file
             outFile.println ();
@@ -34,13 +37,21 @@ public class Warning {
             outFile.println ();
 
             // Process the input file, one token at a time
-            while () {
+            while (scan.hasNext()) {
                 // Get the credit hours and quality points and
                 // determine if the student is on warning. If so,
                 // write the student data to the output file.
-
+                
+                name = scan.next();
+                creditHrs = scan.nextInt();
+                qualityPts = scan.nextDouble();
+                gpa = qualityPts / creditHrs;
+                
+                if((gpa < 1.5 && creditHrs < 30)|| (gpa < 1.75 && creditHrs < 60) || gpa < 2.0)
+                    outFile.println (name + " " + creditHrs + " " + gpa);
             }
             // Close output file
+            outFile.close();
         }
         catch (FileNotFoundException exception) {
             System.out.println ("The file " + inputName + " was not found.");
